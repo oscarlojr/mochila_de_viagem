f101733867161a0685723b7d54ebbb2ecac22be5
Oscar Lima Oliveira Junior <oscarlojr@gmail.com>
2023-05-23 21:07:44 -0300

    Aplicando conceitos de criação de novos elementos na lista

--- js/main.js
+++ js/main.js
@@ -1,13 +1,59 @@
 const form = document.getElementById('novoItem');
+const lista = document.getElementById('lista');
 
 form.addEventListener('submit', (evento) => {
   evento.preventDefault();
-  criaElemento(evento.target.elements['nome'].value, evento.target.elements['quantidade'].value)
+  this.criaElemento(evento.target.elements['nome'].value, evento.target.elements['quantidade'].value);
 });
 
-function criaElemento(nome, quantidade){
 
-  console.log(nome);
-  console.log(quantidade);
+function criaElemento(nome, quantidade) {
 
+  //<li class="item"><strong>3</strong>Casaco</li>
+  const novoItem = document.createElement('li');
+  novoItem.classList.add('item');
+
+  const numeroItem = document.createElement('strong');
+  numeroItem.innerHTML = quantidade;
+
+  novoItem.appendChild(numeroItem);
+  novoItem.innerHTML += nome;
+
+  lista.appendChild(novoItem);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const form = document.getElementById('novoItem');
+
+// form.addEventListener('submit', (evento) => {
+//   evento.preventDefault();
+//   criaElemento(evento.target.elements['nome'].value, evento.target.elements['quantidade'].value)
+// });
+
+// function criaElemento(nome, quantidade){
+
+//   console.log(nome);
+//   console.log(quantidade);
+
+// }
