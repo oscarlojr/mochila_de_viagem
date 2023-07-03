29ccd5c847488757d3b47bc9b251648c070bdd66
Oscar Lima Oliveira Junior <oscarlojr@gmail.com>
2023-07-03 16:38:20 -0300

    Aplicando conceitos de atualização de elementos

--- js/main.js
+++ js/main.js
@@ -1,40 +1,51 @@
 const form = document.getElementById('novoItem');
+const lista = document.getElementById('lista');
 const setItens = JSON.parse(localStorage.getItem('itens')) || [];
 
-setItens.forEach( (elementos) => {
-  this.criaElemento(elementos);
+setItens.forEach( (elemento) => {
+  this.criaElemetos(elemento);
 });
 
 form.addEventListener('submit', (evento) => {
   evento.preventDefault();
   const nome = evento.target.elements['nome'];
   const quantidade = evento.target.elements['quantidade'];
-
+  const existe = setItens.find( elementos => elementos.nome === nome.value);
   const objItens = {
     'nome': nome.value,
-    'quantidade': quantidade.value
+    'quantidade': quantidade.value,
+  };
+
+  if(existe) {
+    objItens.id = existe.id;
+    this.alualizaElementos(objItens);
+    setItens[existe.id] = objItens;
+  } else {
+    objItens.id = setItens.length;
+    this.criaElemetos(objItens);
+    setItens.push(objItens);
   }
+  
 
-  this.criaElemento(objItens);
-
-  setItens.push(objItens);
   localStorage.setItem('itens', JSON.stringify(setItens));
 
   nome.value = '';
   quantidade.value = '';
 });
 
-function criaElemento(elementos) {
-  //<li class="item"><strong>7</strong>Camisas</li>
+function criaElemetos(valorItem) {
   const novoItem = document.createElement('li');
   novoItem.classList.add('item');
 
   const numeroItem = document.createElement('strong');
-  numeroItem.innerHTML = elementos.quantidade;
+  numeroItem.innerHTML = valorItem.quantidade;
+  numeroItem.dataset.id = valorItem.id;
 
   novoItem.appendChild(numeroItem);
-  novoItem.innerHTML += elementos.nome;
-
-  const lista = document.getElementById('lista');
+  novoItem.innerHTML += valorItem.nome;
   lista.appendChild(novoItem);
+};
+
+function alualizaElementos(item) {
+  document.querySelector(`[data-id="${item.id}"]`).innerHTML = item.quantidade;
 }
