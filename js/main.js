30fddf32c7af94607137826903427bd18176ca89
Oscar Lima Oliveira Junior <oscarlojr@gmail.com>
2023-07-06 17:08:28 -0300

    Excluindo elemento do localStorage

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ const lista = document.getElementById('lista');
 const setItens = JSON.parse(localStorage.getItem('itens')) || [];
 
 setItens.forEach( (elemento) => {
-  this.criaElemetos(elemento);
+  this.criaElementos(elemento);
 });
 
 form.addEventListener('submit', (evento) => {
@@ -19,12 +19,12 @@ form.addEventListener('submit', (evento) => {
   };
 
   if(existe) {
-    objItens.id = existe.id
+    objItens.id = existe.id;
     this.atualizaElementos(objItens);
-    setItens[existe.id] = objItens;
+    setItens[setItens.findIndex( elemento => elemento.id === existe.id)] = objItens;
   } else {
-    objItens.id = setItens.length;
-    this.criaElemetos(objItens);
+    objItens.id = setItens[setItens.length - 1] ? setItens[setItens.length - 1].id + 1 : 0 ;
+    this.criaElementos(objItens);
     setItens.push(objItens);
   };
 
@@ -34,35 +34,40 @@ form.addEventListener('submit', (evento) => {
   quantidade.value = '';
 });
 
-function criaElemetos(valorItem) {
+function criaElementos(valorItem) {
   const novoItem = document.createElement('li');
   novoItem.classList.add('item');
-
+  
   const numeroItem = document.createElement('strong');
   numeroItem.innerHTML = valorItem.quantidade;
   numeroItem.dataset.id = valorItem.id;
 
   novoItem.appendChild(numeroItem);
+  
   novoItem.innerHTML += valorItem.nome;
-  novoItem.appendChild(criaBotaoExcluir());
-  lista.appendChild(novoItem);
+  novoItem.appendChild(criaBotaoExcluir(valorItem.id));
+
+  lista.appendChild(novoItem);  
 };
 
 function atualizaElementos(item) {
   document.querySelector(`[data-id="${item.id}"]`).innerHTML = item.quantidade;
 };
 
-function criaBotaoExcluir() {
+function criaBotaoExcluir(id) {
   const elementoBotao = document.createElement('button');
-  elementoBotao.innerText = 'x';
+  elementoBotao.innerText = 'X';
 
   elementoBotao.addEventListener('click', function() {
-    deletaElemento(this.parentNode)
+    deletaElemento(this.parentNode, id);
   });
 
   return elementoBotao;
 };
 
-function deletaElemento(tag){
+function deletaElemento(tag, id) {
   tag.remove();
+  setItens.splice(setItens.findIndex(elemento => elemento.id === id),1);
+
+  localStorage.setItem('itens', JSON.stringify(setItens));
 }
